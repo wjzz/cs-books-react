92dbd190e7c57a4433f4c31a46eb4be4a45a0ad6
wjzz <wjedynak@gmail.com>
2019-06-06 13:06:54 +0200

    Updates

---
+++ precache-manifest.8e3716224968722f2fc88e1f98f41c7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85bc4b3175356b0e6c4b898f91570ac",
+    "url": "/cs-books-react/index.html"
+  },
+  {
+    "revision": "d866fe93a3e6706725c6",
+    "url": "/cs-books-react/static/css/main.d2e1b4c9.chunk.css"
+  },
+  {
+    "revision": "ce32c49ad53499d6923d",
+    "url": "/cs-books-react/static/js/2.10e9dfeb.chunk.js"
+  },
+  {
+    "revision": "d866fe93a3e6706725c6",
+    "url": "/cs-books-react/static/js/main.08a9a048.chunk.js"
+  },
+  {
+    "revision": "cee6147dc17c39f6b6fb",
+    "url": "/cs-books-react/static/js/runtime~main.025a57da.js"
+  }
+]);
